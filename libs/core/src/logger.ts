c8ea8c8571088c92e6da40948ae2861a96406a0b
Tanguy Le Loch <lelochtanguy@gmail.com>
2022-08-15 15:23:56 +0200

    rolling log file daily

--- libs/core/src/logger.ts
+++ libs/core/src/logger.ts
@@ -1,5 +1,8 @@
-import { createLogger, format, transports } from 'winston';
-const { combine, timestamp, printf, colorize } = format;
+import * as winston from 'winston';
+
+import 'winston-daily-rotate-file';
+
+const { combine, timestamp, printf, colorize } = winston.format;
 // set silent true in test to avoid log
 const _isSilent: boolean = process.env.SILENT_LOG && process.env.SILENT_LOG === 'true';
 
@@ -13,22 +16,35 @@ function getFormatFile(moduleName: string, name: string) {
 }
 
 export function createCustomLogger(moduleName: string, name: string) {
-  const logger = createLogger({
+  const transports = [];
+  transports.push(createRollingFileTransport(`log/global-%DATE%.log`, getFormatFile(moduleName, name)));
+  if (moduleName) {
+    transports.push(createRollingFileTransport(`log/module/${moduleName}-%DATE%.log`, getFormatFile(moduleName, name)));
+  }
+  if (process.env.ENV != 'prod') {
+    transports.push(
+      new winston.transports.Console({
+        format: winston.format.combine(getFormatFile(moduleName, name), colorize({ all: true })),
+        level: 'debug',
+      }),
+    );
+  }
+  const logger = winston.createLogger({
     level: 'debug',
     silent: _isSilent,
-    transports: [
-      new transports.Console({
-        format: format.combine(getFormatFile(moduleName, name), colorize({ all: true })),
-        level: 'debug',
-      }),
-      new transports.File({
-        filename: 'log/global.log',
-        level: 'debug',
-        format: getFormatFile(moduleName, name),
-        maxsize: 10000000,
-      }),
-    ],
+    transports,
   });
 
   return logger;
+}
+function createRollingFileTransport(filePattern: string, format) {
+  return new winston.transports.DailyRotateFile({
+    filename: filePattern,
+    datePattern: 'YYYY-MM-DD',
+    zippedArchive: false,
+    maxSize: '5m',
+    maxFiles: '14d',
+    format,
+    level: 'debug',
+  });
 }
