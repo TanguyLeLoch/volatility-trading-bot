86c5042ec3afceb0ec9ae0f4698f56939e145028
Tanguy Le Loch <lelochtanguy@gmail.com>
2022-10-23 17:18:20 +0200

    cors plan

--- apps/module-plan/src/main.ts
+++ apps/module-plan/src/main.ts
@@ -4,9 +4,14 @@ import { moduleName } from './module.info';
 import { PlanModule } from './plan.module';
 
 setModuleName(moduleName);
+
 async function bootstrap() {
-  const app = await NestFactory.create(PlanModule);
+  const app = await NestFactory.create(PlanModule, { cors: true });
   await app.listen(ports[moduleName]);
   console.log(`${moduleName} is running on port: ${ports[moduleName]}`);
 }
-bootstrap();
+
+bootstrap().catch((error) => {
+  console.error(error);
+  process.exit(1);
+});
